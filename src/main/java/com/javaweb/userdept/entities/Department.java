a2d3d922acfacee1d4e8e4c74d634b56eec76b4f
maiarenan <renanmaiabh@gmail.com>
2022-01-11 23:48:24 -0300

    Banco configurado

--- src/main/java/com/javaweb/userdept/entities/Department.java
+++ src/main/java/com/javaweb/userdept/entities/Department.java
@@ -1,10 +1,10 @@
 package com.javaweb.userdept.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_department")
--- src/main/java/com/javaweb/userdept/entities/User.java
+++ src/main/java/com/javaweb/userdept/entities/User.java
@@ -1,13 +1,12 @@
 package com.javaweb.userdept.entities;
 
-
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
